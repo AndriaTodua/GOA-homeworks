570d6ae332c2d8403edf79091898b1af9af94f78
AndriaTodua <andriatodua46@gmail.com>
2024-06-05 16:57:01 +0400

    commit

---
+++ level 04/homework1.py
@@ -0,0 +1,29 @@
+num1 = 2
+num2 = 4
+print(num1 + num2)
+print(num2 - num1)
+print(num1 * num2)
+print(num2 // num1)
+print(num1 ** num2)
+num3 = 5
+num4 = 3
+print(num3 + num4)
+print(num3 - num4)
+print(num3 * num4)
+print(num3 // num4)
+print(num3 ** num4)
+
+print(num3 + num1)
+print(num2 - num4)
+print(num1 * num4)
+print(num3 / num2)
+print(num1 **num3)
+
+
+
+
+
+
+
+
+
---
+++ level 04/homework2.py
@@ -0,0 +1,6 @@
+name = "andria"
+surname = "todua"
+age = 13
+hobby = "programireba da frenburti"
+country = "saqartvelo"
+print ("me" + " " + "var" + " " + name + "," + " " + "chemi" + " " + "gvaria" + " " + surname + "," + " " + "me" + " " + "var" + " " + str(age) + " " + "wlis" + "," + " " + "chemi" + " " + "hobia" + " " + hobby + ","+ " " + "chemi" + " " + "qveyanaa" + " " + country)
